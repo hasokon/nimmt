5f9ae0cee69d3a57b605cd53b28bb6576a35fa40
kentaro@chromebook <kentaro.linux@gmail.com>
2017-08-06 12:33:39 +0900

    Add CardSet that has 104 cards data

---
+++ cardset_test.go
@@ -0,0 +1,14 @@
+package nimmt
+
+import (
+//	"fmt"
+	"testing"
+)
+
+func TestCardSetShuffle(t *testing.T) {
+//	fmt.Println(CardSet)
+//	Shuffle(CardSet)
+
+//	fmt.Println()
+//	fmt.Println(CardSet)
+}
